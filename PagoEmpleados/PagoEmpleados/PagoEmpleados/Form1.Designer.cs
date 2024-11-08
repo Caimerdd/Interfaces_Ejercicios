22a2083bc45d845abdb71284c94092c6bb0cf0fa
Diego David Fonseca Moody <166345558+Diego-Fonseca29@users.noreply.github.com>
2024-11-08 07:19:11 -0600

    Add files via upload

---
+++ PagoEmpleados/PagoEmpleados/PagoEmpleados/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace PagoEmpleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBonif = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.lblRecibir = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpBonificacion = new System.Windows.Forms.GroupBox();
+            this.rad10 = new System.Windows.Forms.RadioButton();
+            this.rad35 = new System.Windows.Forms.RadioButton();
+            this.rad20 = new System.Windows.Forms.RadioButton();
+            this.grpAcciones = new System.Windows.Forms.GroupBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.grpBonificacion.SuspendLayout();
+            this.grpAcciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblBonif
+            // 
+            this.lblBonif.AutoSize = true;
+            this.lblBonif.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBonif.Location = new System.Drawing.Point(106, 9);
+            this.lblBonif.Name = "lblBonif";
+            this.lblBonif.Size = new System.Drawing.Size(285, 25);
+            this.lblBonif.TabIndex = 0;
+            this.lblBonif.Text = "Calculo de Bonificaciones";
+            this.lblBonif.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(45, 59);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(166, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre del empleado:";
+            this.lblNombre.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSueldo.Location = new System.Drawing.Point(45, 106);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(111, 16);
+            this.lblSueldo.TabIndex = 2;
+            this.lblSueldo.Text = "Sueldo básico:";
+            // 
+            // lblRecibir
+            // 
+            this.lblRecibir.AutoSize = true;
+            this.lblRecibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecibir.Location = new System.Drawing.Point(45, 391);
+            this.lblRecibir.Name = "lblRecibir";
+            this.lblRecibir.Size = new System.Drawing.Size(108, 16);
+            this.lblRecibir.TabIndex = 3;
+            this.lblRecibir.Text = "Total a recibir:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Retencion de 5%:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bonificación:";
+            // 
+            // grpBonificacion
+            // 
+            this.grpBonificacion.Controls.Add(this.rad35);
+            this.grpBonificacion.Controls.Add(this.rad20);
+            this.grpBonificacion.Controls.Add(this.rad10);
+            this.grpBonificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpBonificacion.Location = new System.Drawing.Point(48, 146);
+            this.grpBonificacion.Name = "grpBonificacion";
+            this.grpBonificacion.Size = new System.Drawing.Size(148, 142);
+            this.grpBonificacion.TabIndex = 6;
+            this.grpBonificacion.TabStop = false;
+            this.grpBonificacion.Text = "Bonificacion";
+            this.grpBonificacion.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rad10
+            // 
+            this.rad10.AutoSize = true;
+            this.rad10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad10.Location = new System.Drawing.Point(6, 28);
+            this.rad10.Name = "rad10";
+            this.rad10.Size = new System.Drawing.Size(52, 17);
+            this.rad10.TabIndex = 7;
+            this.rad10.TabStop = true;
+            this.rad10.Text = "10%:";
+            this.rad10.UseVisualStyleBackColor = true;
+            // 
+            // rad35
+            // 
+            this.rad35.AutoSize = true;
+            this.rad35.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad35.Location = new System.Drawing.Point(6, 103);
+            this.rad35.Name = "rad35";
+            this.rad35.Size = new System.Drawing.Size(52, 17);
+            this.rad35.TabIndex = 8;
+            this.rad35.TabStop = true;
+            this.rad35.Text = "35%:";
+            this.rad35.UseVisualStyleBackColor = true;
+            // 
+            // rad20
+            // 
+            this.rad20.AutoSize = true;
+            this.rad20.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rad20.Location = new System.Drawing.Point(6, 64);
+            this.rad20.Name = "rad20";
+            this.rad20.Size = new System.Drawing.Size(52, 17);
+            this.rad20.TabIndex = 9;
+            this.rad20.TabStop = true;
+            this.rad20.Text = "20%:";
+            this.rad20.UseVisualStyleBackColor = true;
+            // 
+            // grpAcciones
+            // 
+            this.grpAcciones.Controls.Add(this.btnSalir);
+            this.grpAcciones.Controls.Add(this.btnLimpiar);
+            this.grpAcciones.Controls.Add(this.btnCalcular);
+            this.grpAcciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpAcciones.Location = new System.Drawing.Point(318, 146);
+            this.grpAcciones.Name = "grpAcciones";
+            this.grpAcciones.Size = new System.Drawing.Size(148, 142);
+            this.grpAcciones.TabIndex = 7;
+            this.grpAcciones.TabStop = false;
+            this.grpAcciones.Text = "Acciones:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(37, 22);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(37, 60);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(37, 97);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(147, 315);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(179, 350);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(159, 387);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 471);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grpAcciones);
+            this.Controls.Add(this.grpBonificacion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRecibir);
+            this.Controls.Add(this.lblSueldo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblBonif);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpBonificacion.ResumeLayout(false);
+            this.grpBonificacion.PerformLayout();
+            this.grpAcciones.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBonif;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.Label lblRecibir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpBonificacion;
+        private System.Windows.Forms.RadioButton rad35;
+        private System.Windows.Forms.RadioButton rad20;
+        private System.Windows.Forms.RadioButton rad10;
+        private System.Windows.Forms.GroupBox grpAcciones;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
